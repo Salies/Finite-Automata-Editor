321189c65f8d931b87139caf7124f1a56e288baa
Salies <serezane@protonmail.com>
2023-01-01 23:19:42 -0300

    AutomataCanvas: progresso

--- ts-source/AutomataCanvas.ts
+++ ts-source/AutomataCanvas.ts
@@ -5,6 +5,17 @@ class AutomataCanvas {
     // Variáveis de controle de interface.
     private mousePressed: boolean = false;
     // Menus.
+    // Container "coringa" para todos os menus.
+    // O programa vai trocar o conteúdo desse container para exibir o menu correto.
+    private menuWrapper: HTMLDivElement;
+    // Cada um para um tipo diferente de clique com o botão direito do mouse.
+    private emptyClickMenu: HTMLDivElement;
+    private stateClickMenu: HTMLDivElement;
+    private transitionClickMenu: HTMLDivElement;
+    // Interface de criação de estados.
+    private stateCreationUI: HTMLDivElement;
+    // Tela de criação de transições.
+    private transitionCreationUI: HTMLDivElement;
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
@@ -20,6 +31,24 @@ class AutomataCanvas {
             menuOptions[optionName].classList.add('menu-option');
         });
 
+        // Inicializando o wrapper de menus.
+        this.menuWrapper = document.createElement('div');
+        this.menuWrapper.classList.add('menu-wrapper');
+
+        // Construindo o menu para clique em um lugar vazio da tela.
+        this.emptyClickMenu = document.createElement('div');
+        this.emptyClickMenu.appendChild(menuOptions['Adicionar estado']);
+
+        // Construindo o menu para clique em um estado.
+        this.stateClickMenu = document.createElement('div');
+        this.stateClickMenu.append(
+            menuOptions['Remover estado'], menuOptions['Adicionar transição'], 
+            menuOptions['Marcar/desmarcar como final'], menuOptions['Marcar/desmarcar como inicial']
+        );
+    }
+
+    // Cria e retorna a interface de criação de estados.
+    createStateCreationUI() {
 
     }
 }
